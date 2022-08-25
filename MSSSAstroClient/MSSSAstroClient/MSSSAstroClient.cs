e646a09f8f56bc91dc4953bbcbee5238978cce7f
jeccle <89890866+jeccle@users.noreply.github.com>
2022-08-25 12:47:03 +0800

    Created componts for application within repository. Base third-party DLL created.

---
+++ MSSSAstroClient/MSSSAstroClient/MSSSAstroClient.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MSSSAstroClient
+{
+    public partial class MSSSAstroClient : Form
+    {
+        public MSSSAstroClient()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- MSSSAstroClient/MSSSAstroClient/Program.cs
+++ MSSSAstroClient/MSSSAstroClient/Program.cs
@@ -16,7 +16,7 @@ namespace MSSSAstroClient
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MSSSAstroClient());
         }
     }
 }
