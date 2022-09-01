f2721c6efd43a2d11524ae2301c1d6e9c3053f06
jeccle <89890866+jeccle@users.noreply.github.com>
2022-09-01 13:37:47 +0800

    Delete DLL/AstroDLL directory

--- DLL/AstroDLL/AstroMathDriver/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿using AstroMath;
-
-AstroCalculations library = new AstroCalculations();
-Console.WriteLine(library.CalcStarVelocity(500.1, 500.0));
-Console.WriteLine(library.CalcStarDistance(0.547));
-Console.WriteLine(library.CalcKelvin(27));
-Console.WriteLine(library.CalcEventHorizon(8.2 * Math.Pow(10, 36)));
-
